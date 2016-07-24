5a25d43731f5f67d2683b4dc1842988a887ca20e
Eric Renfro <psi-jack@linux-help.org>
2016-07-24 15:59:58 -0400

    Fixed metadata once again

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,15 @@ maintainer_email 'psi-jack@linux-help.org'
 license          'Apache 2.0'
 description      'Installs and configures the NUX Community Desktop Yum Repository'
 long_description ''
-version          '0.1.0'
-issues_url       'http://gogs.home.ld/Linux-Help/cookbook-apt-zabbix/issues'
-source_url       'http://gogs.home.ld/Linux-Help/cookbook-apt-zabbix'
+version          '0.1.1'
+issues_url       'https://git.linux-help.org/Linux-Help/cookbook-apt-atomic/issues'
+source_url       'https://git.linux-help.org/Linux-Help/cookbook-apt-stomic'
 
 %w{ debian }.each do |os|
 	supports os, '>= 5.0'
+end
+%w{ ubuntu }.each do |os|
+	supports os, '>= 12.04'
 end
 
 depends 'apt', '>= 3.0'
