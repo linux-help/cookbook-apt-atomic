d3219c1b27ec5d13a370b7fe9bf95b1c61fed52d
Eric Renfro <psi-jack@linux-help.org>
2016-07-24 16:01:11 -0400

    Fixed debian minimum version

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,12 @@ maintainer_email 'psi-jack@linux-help.org'
 license          'Apache 2.0'
 description      'Installs and configures the NUX Community Desktop Yum Repository'
 long_description ''
-version          '0.1.1'
+version          '0.1.2'
 issues_url       'https://git.linux-help.org/Linux-Help/cookbook-apt-atomic/issues'
 source_url       'https://git.linux-help.org/Linux-Help/cookbook-apt-stomic'
 
 %w{ debian }.each do |os|
-	supports os, '>= 5.0'
+	supports os, '>= 7.0'
 end
 %w{ ubuntu }.each do |os|
 	supports os, '>= 12.04'
